398204bb56a5050e172312449d80a7a5322308d6
Ruan <ruan.natanfsilva@gmail.com>
2022-10-06 23:14:47 -0300

    Projeto Finalizado V2.0

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function logar(){
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+    
+     if(nome == "admin" && email == "admin" ){
+       alert("Seus dados foram salvos, Aguarde contato.");
+       location.href = "Saq.html";
+     } else {
+       alert("Ops, Algo deu errado. tente novamente.")
+     }
+}
